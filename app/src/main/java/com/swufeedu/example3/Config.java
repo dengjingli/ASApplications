02d443999365d66f5bec55d3c2b85eeb271420f6
dengjingli <a15870163338@qq.com>
2021-09-28 12:10:03 +0800

    子线程，网络url

--- app/src/main/java/com/swufeedu/example3/Config.java
+++ app/src/main/java/com/swufeedu/example3/Config.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 public class Config extends AppCompatActivity {
     TextView dollarText,eurText;
+    private static final String TAG="ConfigActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,14 +21,19 @@ public class Config extends AppCompatActivity {
         Intent intent =getIntent();
         float dollar2 =intent.getFloatExtra("dollar_rate_key",0.0f);
         float eur2 =intent.getFloatExtra("eur_rate_key",0.0f);
+        Log.i(TAG,"onCreate:dollar2="+dollar2);
+        Log.i(TAG,"onCreate:dollar2="+eur2);
         dollarText=findViewById(R.id.dollor);
         eurText=findViewById(R.id.eur);
+        //将汇率填入控件中
         dollarText.setText(String.valueOf(dollar2));
         eurText.setText(String.valueOf(eur2));
     }
     public void save(View btn) {
+        //重新获取新的汇率数据
         float dollar =Float.parseFloat(dollarText.getText().toString());
         float eur =Float.parseFloat(eurText.getText().toString());
+
         Intent first=getIntent();
         first.putExtra("dollar_key",dollar);
         first.putExtra("eur_key",eur);
--- app/src/main/java/com/swufeedu/example3/Huilv.java
+++ app/src/main/java/com/swufeedu/example3/Huilv.java
@@ -1,9 +1,16 @@
 package com.swufeedu.example3;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -11,36 +18,64 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
-public class Huilv extends AppCompatActivity {
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Huilv extends AppCompatActivity implements Runnable{
     private static final String TAG="HuiActivity";
     private float dollarRate = 0.15f;
     private float eurRate = 0.13f;
+    TextView show;
+    Handler handler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_huilv);
+
+        //读取保存的数据
+        SharedPreferences sp=getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+        dollarRate=sp.getFloat("dollar_rate",0.1f);
+        eurRate=sp.getFloat("eur_rate",0.1f);
+        Log.i(TAG,"onCreate:get from sp dollar="+dollarRate);
+        Log.i(TAG,"onCreate:get from sp eur="+eurRate);
+        //开启线程
+        handler=new Handler(Looper.myLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg){
+                Log.i(TAG,"handleMessge:接收消息");
+                if(msg.what==6){
+                    String str=(String)msg.obj;
+                    Log.i(TAG,"handleMessage:str="+str);
+                }
+                super.handleMessage(msg);
+            }
+        };
+        Thread t = new Thread(this);
+        t.start();
+
     }
-    public void click(View btn){
+    public void huiclick(View btn){
         Log.i(TAG,"click");
         EditText inputText = findViewById(R.id.input);
         String text =inputText.getText().toString();
         if(text!=null&&text.length()>0){
             float num =Float.parseFloat(text);
             float res = 0;
-            if(btn.getId()==R.id.button2){
+            if(btn.getId()==R.id.butdollar){
                 res = num*dollarRate;
             }
             else if(btn.getId()==R.id.button3){
                 res = num*eurRate;
             }
-
-            TextView show = findViewById(R.id.result);
+            show = findViewById(R.id.result);
             show.setText(String.valueOf(res));
-
         }
-
-
     }
     public void openConfig(View btn) {
         config();
@@ -55,14 +90,20 @@ public class Huilv extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult (int requestCode,int resultCode,Intent data){
+    protected void onActivityResult (int requestCode,int resultCode,@NonNull Intent data){
         if(requestCode==1&&resultCode==2){
             Bundle bundle = data.getExtras();
-            dollarRate=bundle.getFloat("key_dollar",0.1f);
-            eurRate=bundle.getFloat("key_eur",0.1f);
+            dollarRate=bundle.getFloat("dollar_key",0.1f);
+            eurRate=bundle.getFloat("eur_key",0.2f);
+
+            //保存数据到sp
+            SharedPreferences sp=getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+            SharedPreferences.Editor editor =sp.edit();
+            editor.putFloat("dollar_rate",dollarRate);
+            editor.putFloat("eur_rate",eurRate);
+            editor.apply();
         }
         super.onActivityResult(requestCode,resultCode,data);
-
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu){
@@ -70,11 +111,59 @@ public class Huilv extends AppCompatActivity {
         return true;
     }
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(@NonNull MenuItem item){
         if(item.getItemId()==R.id.menu_setting){
             Log.i(TAG,"onOptionsItemSelected:setting");
             config();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        Log.i(TAG,"run:rum()......");
+
+        //获取网络数据
+        URL url =null;
+
+        try {
+            url = new URL("https://www.usd-cny.com/bankofchina.htm");
+            HttpURLConnection http =(HttpURLConnection)url.openConnection();
+            InputStream in = http.getInputStream();
+
+            String html = inputStream2String(in);
+            Log.i(TAG,"run:html="+html);
+        }catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        //发送消息给主线程
+        Message msg=handler.obtainMessage();
+        msg.what=6;
+        msg.obj="Hello from run";
+        handler.sendMessage(msg);
+        Log.i(TAG,"run:消息已发送");
+    }
+    private String inputStream2String(InputStream inputStream)
+        throws IOException{
+        final int bufferSize =1024;
+        final char[] buffer=new char[bufferSize];
+        final  StringBuilder out =new StringBuilder();
+        Reader in = new InputStreamReader(inputStream,"gb2312");
+        while (true){
+            int rsz=in.read(buffer,0,buffer.length);
+            if(rsz<0)
+                break;
+            out.append(buffer,0,rsz);
+        }
+        return out.toString();
     }
 }
